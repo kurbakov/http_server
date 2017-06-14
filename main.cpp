058bc7b200a083907e41097f4136f9dcfc3a82b1
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2017-06-14 22:02:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <unistd.h>
 #include <fstream>
 
 struct message {
@@ -12,7 +18,8 @@ struct message {
 int main()
 {
     // get the key
-    key_t key = ftok("/tmp/msg.temp", 1);
+    int key = ftok("/tmp/msg.temp", 1);
+    std::cout << key << "\n";
 
     // create queue
     int queue_code = msgget(key, IPC_CREAT|IPC_EXCL|0666);
@@ -21,37 +28,30 @@ int main()
     std::ofstream output_file("/home/box/message.txt");
 
 //    if(fork()) {
-//        message msg;
-//        std::strcpy(msg.mtext, "my message is here!");
-//        msg.mtype = 1;
+//        message msg_send;
+//        std::strcpy(msg_send.mtext, "my message is here!");
+//        msg_send.mtype = 1;
 //
-//        ssize_t rc = msgsnd(queue_code, &msg, sizeof(msg.mtext), 0);
+//        ssize_t rc = msgsnd(queue_code, &msg_send, sizeof(msg_send.mtext), 0);
 //        if (rc < 0) {
+//            perror( strerror(errno) );
 //            std::cout << "msgsnd failed, rc = " << rc << "\n";
 //            return 1;
 //        }
 //    }
-//    sleep(1);
-
-    if(fork()){
-        message msg;
-        ssize_t rc = msgrcv(queue_code, (void *) &msg, sizeof(msg.mtext), 0, 0);
+//
+    if(fork()) {
+        message msg_recv;
+        ssize_t rc = msgrcv(queue_code, &msg_recv, sizeof(msg_recv.mtext), 0, 0);
 
         if (rc < 0) {
+            perror(strerror(errno));
             std::cout << "msgrcv failed, rc = " << rc << "\n";
             return 1;
         }
-        std::cout << msg.mtext << "\n";
-    }
 
-    // remove queue
-//    return_code = msgctl(queue_code, IPC_RMID, NULL);
-//    if (return_code < 0) {
-//        perror( strerror(errno) );
-//        printf("msgctl (return queue) failed, rc=%d\n", return_code);
-//        return 1;
-//    }
-//    printf("message queue %d is gone\n", return_code);
+        std::cout << msg_recv.mtext << "\n";
+    }
 
     return 0;
 }
