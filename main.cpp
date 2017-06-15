058e3aa910dd8d47b54f3486f3705fd49bc75fb2
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2017-06-15 20:17:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,27 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <mqueue.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <iostream>
-#include <fstream>
 
-#define MSG_SIZE 4096
-
-int main(int argc, char **argv)
+int main(int argc, char const *argv[])
 {
-    char buffer[MSG_SIZE + 1];
+	// get the key of the queue
+	key_t key = ftok("/temp/sem.temp", 1);
 
-    // initialize the queue attributes
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = MSG_SIZE;
-    attr.mq_curmsgs = 0;
+	// create sim
+	int sem_id = semget(key, 16, IPC_CREAT | 0666);
 
-    // create file
-    std::ofstream output_file("/home/box/message.txt");
+	union semun {
+		int val;
+		struct semid_ds *buf;
+ 		ushort *array;
+ 	} arg;
 
-    /* create the message queue */
-    mqd_t mq = mq_open("/test.mq", O_CREAT | O_RDWR, 0666, &attr);
-
-    if(fork()){
-        ssize_t bytes_read = mq_receive(mq, buffer, MSG_SIZE, NULL);
-
-        buffer[bytes_read] = '\0';
-        output_file << buffer;
+	for(int i=0; i<16; i++)
+	{
+		arg.val = i;
+		semctl(sem_id, i, SETVAL, arg);
 	}
 
-    // mq_close(mq);
-
-    return 0;
+	return 0;
 }
