89bc28772b494c532eb3f329aabddcb15ac4f9bf
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2017-06-15 22:19:12 +0200

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <cstring>
 
-const int SIZE = 4 * 1024*1024; /*4 MB*/
+const int SIZE = 2 * 1024*1024; /*2 MB*/
 
 int main(int argc, char const *argv[])
 {
@@ -12,12 +12,16 @@ int main(int argc, char const *argv[])
 	key_t key = ftok("/tmp/mem.temp",1);
 
 	// create shm
-	int shm_id = shmget(key, SIZE , IPC_CREAT | IPC_EXCL | 0666);
+	int shm_id = shmget(key, SIZE , IPC_CREAT | IPC_EXCL | 0600);
+	if(shm_id < 0)
+		std::cout << "shmget failed" << "\n";
 
 	// write data
-	char data = '42';
-	void* it = shmat(key, NULL, 0);
-	memcpy((char*)it, data, 10);
+	void* it = shmat(shm_id, NULL, 0);
+	if(it == (void*)-1)
+		std::cout << "shmat failed" << "\n";
+
+	memcpy(it, "42", 1024*1024+10);
 
 	return 0;
 }
