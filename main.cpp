b74e25d5efe6ba817c5ffeec8529f37b3c5807e9
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2017-06-16 21:45:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,84 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <iostream>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h> // sleep()
 
-const int SIZE = 2 * 1024*1024; /*2 MB*/
+
+void* func_mutex(void* mutex)
+{
+	pthread_mutex_t* m;
+	m = (pthread_mutex_t*) mutex;
+	
+	int rc_mutex;
+	std::cout << "waiting for lock..." <<"\n";
+	rc_mutex = pthread_mutex_lock(m);
+
+	std::cout << "lock is done.." <<"\n";
+	rc_mutex = pthread_mutex_unlock(m);
+
+	return NULL;
+}
+
+
+void* func_spin(void* spin)
+{
+	// pthread_spin_t* s;
+	// s = (pthread_spin_t*) spin;
+	
+	// int rc_spin;
+	// std::cout << "waiting for spin..." <<"\n";
+	// rc_spin = pthread_spin_lock(s);
+
+	// std::cout << "spin is done.." <<"\n";
+	// rc_spin = pthread_spin_unlock(s);
+
+	return NULL;
+}
+
+
+
+void* func_read();
+void* func_write();
 
 int main(int argc, char const *argv[])
 {
-	// get the key of the queue
-	key_t key = ftok("/tmp/mem.temp",1);
+	pthread_t thread1, thread2, thread3, thread4;
 
-	// create shm
-	int shm_id = shmget(key, SIZE , IPC_CREAT | IPC_EXCL | 0600);
-	if(shm_id < 0)
-		std::cout << "shmget failed" << "\n";
+// -------------------------------------------------------------------------------
 
-	// write data
-	void* it = shmat(shm_id, NULL, 0);
-	if(it == (void*)-1)
-		std::cout << "shmat failed" << "\n";
+	// mutex
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-	memcpy(it, "42", 1024*1024+10);
+	int rc_mutex;
+	rc_mutex = pthread_mutex_init(&mutex, NULL);
+	rc_mutex = pthread_mutex_lock(&mutex);
+
+	int p_status_mutex = pthread_create(&thread1, NULL, func_mutex, &mutex);
+	sleep(5);
+
+	rc_mutex = pthread_mutex_unlock(&mutex);
+	rc_mutex = pthread_mutex_destroy(&mutex);
+// -------------------------------------------------------------------------------
+
+	// spin
+	pthread_spinlock_t spin;
+
+	int rc_spin;
+	// rc_spin = pthread_spin_init(&spin, PTHREAD_PROCESS_SHARED); 
+	// rc_spin = pthread_spin_lock(&spin);
+
+	int p_status_spin = pthread_create(&thread2, NULL, func_spin, &spin);
+	// sleep(5);
+
+	// rc_spin = pthread_spin_unlock(&spin);
+	// rc_spin = pthread_spin_destroy(&spin);
+
+// -------------------------------------------------------------------------------
+
+	// join all threads:
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 
 	return 0;
 }
